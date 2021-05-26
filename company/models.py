c727289f48b0c36969af6486201a37af79fa85b9
Ives Costa <ivespauiniam@gmail.com>
2021-05-26 00:35:25 -0500

    APP company has created

---
+++ company/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, help_text='Name of company')
+
+    def __str__(self):
+        return 'Nome Empresa:' +  str(self.name)
