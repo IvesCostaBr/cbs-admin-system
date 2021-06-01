5378d0cae37cdf23656b0700575cde371d648924
Ives Costa <ivespauiniam@gmail.com>
2021-06-01 03:38:05 -0500

    celery

--- admin_system/celery.py
+++ admin_system/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_system.settings')
 
-app = Celery('proj')
+app = Celery('admin_system')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
